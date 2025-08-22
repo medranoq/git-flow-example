e16128f935b07c9019fff9d4a4118a999a4b8f00
Raul Medrano <medranoq@outlook.com>
2025-08-22 12:13:51 -0700

    fastapi docker

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+__version__ = "0.2.0"
+
+app = FastAPI(title="GitFlow Example API", version=__version__)
+
+@app.get("/")
+def root():
+    return {"message": f"Hello world! This is version {__version__}."}
+
